b4e76f630ec57b30075c5bbaa710bea591697514
Calvin Ho <calvin.ch.ho@gmail.com>
2022-07-07 10:40:49 -0400

    fix map-show script

--- public/scripts/maps_show.js
+++ public/scripts/maps_show.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-
-// Either get GeoSearch from the window global, or import from `leaflet-geosearch`
-// import * as GeoSearch from 'leaflet-geosearch';
-// const map = L.map('map').setView([51.505, -0.09], 13);
-// L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-// const search = new GeoSearch.GeoSearchControl({
-//   provider: new GeoSearch.OpenStreetMapProvider(),
-// });
-// map.addControl(search);
-
-
   console.log(map_id);
   let initialLat = 43.7;
   let initialLong = -79.4;
